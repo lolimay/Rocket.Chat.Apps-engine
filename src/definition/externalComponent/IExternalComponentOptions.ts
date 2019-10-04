61c15701572c1ad3f6db5bda8824d7539437085d
lolimay <lolimay@lolimay.cn>
2019-10-04 09:15:26 +0800

    Remove IExternalComponentOptions and IWebhooks

--- src/definition/externalComponent/IExternalComponentOptions.ts
+++
@@ -1,6 +0,0 @@
-import { IWebHooks } from './IWebHooks';
-
-export interface IExternalComponentOptions {
-    // The webhooks options. Please see IWebHooks.ts for the detail.
-    webhooks: IWebHooks;
-}
