9d62a3f573ff60739df65f691f18d80239d71603
lolimay <lolimay@lolimay.cn>
2019-10-15 09:45:12 +0800

    revert changes in IInternalBridge

--- src/server/bridges/IInternalBridge.ts
+++ src/server/bridges/IInternalBridge.ts
@@ -3,10 +3,4 @@ import { ISetting } from '../../definition/settings';
 export interface IInternalBridge {
     getUsernamesOfRoomById(roomId: string): Array<string>;
     getWorkspacePublicKey(): Promise<ISetting>;
-    /**
-     * Get user's avatar URL by providing his username
-     *
-     * @param username The user's username corresponding to the avatar
-     */
-    getUserAvatarURLByUsername(username: string): string;
 }
